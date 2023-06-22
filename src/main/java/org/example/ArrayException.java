5980b18e7766a3dd20ca8460f1ffd4a9114d7a95
Elvira <elviraw@mail.ru>
2023-06-22 20:50:00 +0300

    added row and column variables to the class ArrayException

--- src/main/java/org/example/ArrayException.java
+++ src/main/java/org/example/ArrayException.java
@@ -3,6 +3,10 @@ package org.example;
 public abstract class ArrayException extends Exception {
     private String[][] array;
 
+    private int rows;
+
+    private int columns;
+
     public String[][] getArray() {
         return array;
     }
@@ -10,5 +14,7 @@ public abstract class ArrayException extends Exception {
     public ArrayException(String message, String[][] array, int rows, int columns) {
         super(message);
         this.array = array;
+        this.rows = rows;
+        this.columns = columns;
     }
 }
